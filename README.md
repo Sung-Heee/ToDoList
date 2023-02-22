d8fe6839159d8bdf6ac04050c56bc474e651904d
HeeHeeHee <praaa0220@gmail.com>
2023-02-22 22:29:38 +0900

    배경 사이즈 다시  설정

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ToDoList
+
+### 기능 
+- [x] 실시간 시계 (요일/시간)
+- [x] 배경 이미지 새로고침시 랜덤으로 변경  
+- [x] Local Storage를 사용한 로그인  
+- [x] Local Storage를 사용한 ToDoList  
+- [x] 날씨와 위치 (geolocation)
+- [x] 페이지 하단 명언  
+
+
+### 추가 구현 기능  
+- [ ] 할 일 수정 기능  
+- [ ] 할 일 복원 기능  
--- js/background.js
+++ js/background.js
@@ -1,7 +1,16 @@
 //랜덤하게 배경이미지 바뀌게 하기.
 //array에서 랜덤한 숫자를 얻고, 그 랜덤한 숫자에 따른 배경이미지를 body에 추가.
 
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
+const images = [
+  "0.jpeg",
+  "1.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+];
 //const images = ["4.jpeg"];
 const BGI = "background";
 
